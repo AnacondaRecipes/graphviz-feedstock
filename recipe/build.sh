9362db3940e703268763a90a8588c564452ec335
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-03-09 21:51:39 +0200

    test: fixed -Ttiff support; deps reworked

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ set -x
 
 # https://github.com/conda-forge/xorg-libxfixes-feedstock/issues/13
 export PKG_CONFIG_PATH=$PREFIX/share/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
 
 ./autogen.sh
 
@@ -14,11 +15,20 @@ sed -i.bak '/Requires.private:/d' $PREFIX/lib/pkgconfig/librsvg*.pc
 
 declare -a _xtra_config_flags
 
-if [[ "${target_platform}" =~ .*osx.* ]]; then
+if [[ "${target_platform}" == osx-* ]]; then
     export OBJC="${CC}"
     # xcodebuild uses ld instead of clang and fails
     export LD="${CC_FOR_BUILD}"
     _xtra_config_flags+=(--with-quartz)
+    _xtra_config_flags+=(--without-gtk)
+    _xtra_config_flags+=(--without-gdk)
+    _xtra_config_flags+=(--without-gdk-pixbuf)
+fi
+
+if [[ "${target_platform}" == linux-* ]]; then
+    _xtra_config_flags+=(--with-gtk=yes)
+    _xtra_config_flags+=(--with-gdk=yes)
+    _xtra_config_flags+=(--with-gdk-pixbuf=yes)
 fi
 
 # -----------------------------------------------------------------------------
@@ -26,8 +36,19 @@ fi
 # Newer clang (macOS) and gcc emit many -Wsign-conversion, -Wfloat-equal, etc.
 # We explicitly disable the noisy warning classes.
 # -----------------------------------------------------------------------------
-export CFLAGS="${CFLAGS} -Wno-sign-conversion -Wno-float-equal -Wno-switch-default -Wno-unused-but-set-variable -Wno-cast-qual -Wno-bad-function-cast -Wno-implicit-int-float-conversion -Wno-implicit-int-conversion -Wno-shorten-64-to-32 -Wno-float-conversion -Wno-implicit-float-conversion -Wno-unused-parameter"
-
+export CFLAGS="${CFLAGS} \
+  -Wno-sign-conversion \
+  -Wno-float-equal \
+  -Wno-switch-default \
+  -Wno-unused-but-set-variable \
+  -Wno-cast-qual \
+  -Wno-bad-function-cast \
+  -Wno-implicit-int-float-conversion \
+  -Wno-implicit-int-conversion \
+  -Wno-shorten-64-to-32 \
+  -Wno-float-conversion \
+  -Wno-implicit-float-conversion \
+  -Wno-unused-parameter"
 # -----------------------------------------------------------------------------
 # Fix permission issues for Python code generators used during build.
 # Graphviz Makefiles try to execute these scripts directly.
@@ -43,7 +64,6 @@ chmod +x "$SRC_DIR/lib/common/make_colortbl.py" \
             --enable-ltdl \
             --without-x \
             --without-qt \
-            --without-gtk \
             --enable-ruby=no \
             --enable-swig=no \
             --enable-java=no \
@@ -58,13 +78,11 @@ chmod +x "$SRC_DIR/lib/common/make_colortbl.py" \
             --with-demos=no \
             --with-ann=no \
             --with-gts=yes \
-            --with-gdk=yes \
             --with-rsvg=yes \
             --with-expat=yes \
             --with-libgd=yes \
             --with-freetype2=yes \
             --with-pangocairo=yes \
-            --with-gdk-pixbuf=yes \
             "${_xtra_config_flags[@]}"
 
 make -j"${CPU_COUNT}"
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -9,9 +9,20 @@ sfdp -V
 neato -?
 
 dot -T? || true
-dot -Tpng -o sample.png sample.dot
-dot -Tpdf -o sample.pdf sample.dot
-dot -Tsvg -o sample.svg sample.dot
+
+# Render basics (dot)
+dot -Tpng  -o sample.png  sample.dot
+dot -Tpdf  -o sample.pdf  sample.dot
+dot -Tsvg  -o sample.svg  sample.dot
 dot -Tjpeg -o sample.jpeg sample.dot
-sfdp -Tpdf -o sample.pdf sample.dot
+dot -Tgif  -o sample.gif  sample.dot
 dot -Ttiff -o sample.tiff sample.dot
+dot -Twebp -o sample.webp sample.dot
+dot -Teps  -o sample.eps  sample.dot
+dot -Tps   -o sample.ps   sample.dot
+dot -Tps2  -o sample.ps2  sample.dot
+sfdp -Tpdf -o sfdp.pdf sample.dot
+
+# sanity: check if files created and not empty
+for f in sample.*; do test -s "$f"; done
+test -s sfdp.pdf
