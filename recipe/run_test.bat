a65d7d0c132b5bbc4f1697239b186e066448381d
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-03-10 14:32:36 +0200

    test: add more check and tests

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,28 +1,154 @@
-dot -c
-if errorlevel 1 exit 1
+@echo on
+setlocal EnableExtensions EnableDelayedExpansion
 
-dot -V
-if errorlevel 1 exit 1
+REM ----------------------------
+REM All expected executables exist
+REM ----------------------------
+if not exist "%LIBRARY_BIN%\acyclic.exe" exit 1
+if not exist "%LIBRARY_BIN%\bcomps.exe" exit 1
+if not exist "%LIBRARY_BIN%\ccomps.exe" exit 1
+if not exist "%LIBRARY_BIN%\circo.exe" exit 1
+if not exist "%LIBRARY_BIN%\cluster.exe" exit 1
+if not exist "%LIBRARY_BIN%\diffimg.exe" exit 1
+if not exist "%LIBRARY_BIN%\dijkstra.exe" exit 1
+if not exist "%LIBRARY_BIN%\dot.exe" exit 1
+if not exist "%LIBRARY_BIN%\dot_builtins.exe" exit 1
+if not exist "%LIBRARY_BIN%\dot_sandbox.exe" exit 1
+if not exist "%LIBRARY_BIN%\dot2gxl.exe" exit 1
+if not exist "%LIBRARY_BIN%\edgepaint.exe" exit 1
+if not exist "%LIBRARY_BIN%\fdp.exe" exit 1
+if not exist "%LIBRARY_BIN%\gc.exe" exit 1
+if not exist "%LIBRARY_BIN%\gml2gv.exe" exit 1
+if not exist "%LIBRARY_BIN%\graphml2gv.exe" exit 1
+if not exist "%LIBRARY_BIN%\gv2gml.exe" exit 1
+if not exist "%LIBRARY_BIN%\gv2gxl.exe" exit 1
+if not exist "%LIBRARY_BIN%\gvcolor.exe" exit 1
+if not exist "%LIBRARY_BIN%\gvgen.exe" exit 1
+if not exist "%LIBRARY_BIN%\gvmap.exe" exit 1
+if not exist "%LIBRARY_BIN%\gvpack.exe" exit 1
+if not exist "%LIBRARY_BIN%\gvpr.exe" exit 1
+if not exist "%LIBRARY_BIN%\gxl2dot.exe" exit 1
+if not exist "%LIBRARY_BIN%\gxl2gv.exe" exit 1
+if not exist "%LIBRARY_BIN%\mm2gv.exe" exit 1
+if not exist "%LIBRARY_BIN%\neato.exe" exit 1
+if not exist "%LIBRARY_BIN%\nop.exe" exit 1
+if not exist "%LIBRARY_BIN%\osage.exe" exit 1
+if not exist "%LIBRARY_BIN%\patchwork.exe" exit 1
+if not exist "%LIBRARY_BIN%\prune.exe" exit 1
+if not exist "%LIBRARY_BIN%\sccmap.exe" exit 1
+if not exist "%LIBRARY_BIN%\sfdp.exe" exit 1
+if not exist "%LIBRARY_BIN%\tred.exe" exit 1
+if not exist "%LIBRARY_BIN%\twopi.exe" exit 1
+if not exist "%LIBRARY_BIN%\unflatten.exe" exit 1
 
-dot -v < NUL
-if errorlevel 1 exit 1
+REM Optional: quick run sanity (aliases)
+"%LIBRARY_BIN%\circo.exe" -V || exit 1
+"%LIBRARY_BIN%\fdp.exe"   -V || exit 1
+"%LIBRARY_BIN%\neato.exe" -V || exit 1
+"%LIBRARY_BIN%\osage.exe" -V || exit 1
+"%LIBRARY_BIN%\patchwork.exe" -V || exit 1
+"%LIBRARY_BIN%\sfdp.exe"  -V || exit 1
+"%LIBRARY_BIN%\twopi.exe" -V || exit 1
 
-fdp -V
-if errorlevel 1 exit 1
+REM ----------------------------
+REM Core headers exist
+REM ----------------------------
+if not exist "%LIBRARY_INC%\graphviz" exit 1
+if not exist "%LIBRARY_INC%\graphviz\arith.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\cdt.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\cgraph.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\color.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\geom.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\graphviz_version.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvc.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvcext.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvcjob.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvcommon.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvconfig.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvplugin.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvplugin_device.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvplugin_layout.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvplugin_loadimage.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvplugin_render.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvplugin_textlayout.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\gvpr.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\pack.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\pathgeom.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\pathplan.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\textspan.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\types.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\usershape.h" exit 1
+if not exist "%LIBRARY_INC%\graphviz\xdot.h" exit 1
 
-sfdp -V
-if errorlevel 1 exit 1
+REM ----------------------------
+REM Core libraries exist (import libs and/or dlls)
+REM ----------------------------
+if not exist "%LIBRARY_LIB%\gvc.lib" exit 1
+if not exist "%LIBRARY_LIB%\cgraph.lib" exit 1
+if not exist "%LIBRARY_LIB%\cdt.lib" exit 1
+if not exist "%LIBRARY_LIB%\pathplan.lib" exit 1
+if not exist "%LIBRARY_LIB%\gvpr.lib" exit 1
+if not exist "%LIBRARY_LIB%\xdot.lib" exit 1
 
-neato -?
-if errorlevel 1 exit 1
+REM Runtime DLLs (names can vary by version; check the common ones)
+if not exist "%LIBRARY_BIN%\gvc.dll" exit 1
+if not exist "%LIBRARY_BIN%\cgraph.dll" exit 1
+if not exist "%LIBRARY_BIN%\cdt.dll" exit 1
 
-for %%t in (png, pdf, svg, tiff, jpeg) do (
-    dot -T%%t -o sample.%%t sample.dot
-    if errorlevel 1 exit 1
+REM ----------------------------
+REM Plugin libraries exist
+REM ----------------------------
+if not exist "%LIBRARY_LIB%\graphviz" exit 1
+
+REM plugins are dll on Windows
+if not exist "%LIBRARY_LIB%\graphviz\gvplugin_core.dll" exit 1
+if not exist "%LIBRARY_LIB%\graphviz\gvplugin_dot_layout.dll" exit 1
+if not exist "%LIBRARY_LIB%\graphviz\gvplugin_neato_layout.dll" exit 1
+if not exist "%LIBRARY_LIB%\graphviz\gvplugin_gd.dll" exit 1
+if not exist "%LIBRARY_LIB%\graphviz\gvplugin_pango.dll" exit 1
+if not exist "%LIBRARY_LIB%\graphviz\gvplugin_webp.dll" exit 1
+
+REM optional (depending on build options)
+REM if not exist "%LIBRARY_LIB%\graphviz\gvplugin_gdk.dll" exit 1
+REM if not exist "%LIBRARY_LIB%\graphviz\gvplugin_rsvg.dll" exit 1
+
+REM config file is required so plugins load
+if not exist "%LIBRARY_LIB%\graphviz\config" exit 1
+if not exist "%LIBRARY_LIB%\graphviz\config6" if not exist "%LIBRARY_LIB%\graphviz\config7" if not exist "%LIBRARY_LIB%\graphviz\config8" (
+  REM some builds use "config" only, but keep this to avoid false negatives
 )
 
-sfdp -Tpdf -o sample.pdf sample.dot
-if errorlevel 1 exit 1
+REM ----------------------------
+REM Basic plugin config + versions
+REM ----------------------------
+"%LIBRARY_BIN%\dot.exe" -c || exit 1
+"%LIBRARY_BIN%\dot.exe" -V || exit 1
 
-call dot.bat -V
-if errorlevel 1 exit 1
+REM Show formats (non-fatal)
+"%LIBRARY_BIN%\dot.exe" -T? || echo dot -T? failed (non-fatal)
+
+REM ----------------------------
+REM Render outputs
+REM ----------------------------
+"%LIBRARY_BIN%\dot.exe" -Tpng  -o sample.png  sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Tpdf  -o sample.pdf  sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Tsvg  -o sample.svg  sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Tjpeg -o sample.jpeg sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Tgif  -o sample.gif  sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Ttiff -o sample.tiff sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Twebp -o sample.webp sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Teps  -o sample.eps  sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Tps   -o sample.ps   sample.dot || exit 1
+"%LIBRARY_BIN%\dot.exe" -Tps2  -o sample.ps2  sample.dot || exit 1
+
+"%LIBRARY_BIN%\sfdp.exe" -Tpdf -o sfdp.pdf sample.dot || exit 1
+
+REM ----------------------------
+REM Sanity: outputs are non-empty
+REM ----------------------------
+for %%F in (sample.png sample.pdf sample.svg sample.jpeg sample.gif sample.tiff sample.webp sample.eps sample.ps sample.ps2 sfdp.pdf) do (
+  if not exist "%%F" exit 1
+  for %%S in ("%%F") do if %%~zS LSS 1 exit 1
+)
+
+exit 0
